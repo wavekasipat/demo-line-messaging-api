652154fda8eb1e1f6959f268e990fd3fc7d72618
wavekasipat <wavekasipat@gmail.com>
2024-10-27 13:19:45 +0700

    crypto

--- src/lib/validateLineSignature.js
+++ src/lib/validateLineSignature.js
@@ -1,4 +1,4 @@
-const crypto = require("crypto");
+import crypto from "crypto";
 
 export const validateLineSignature = (body, reqSignature) => {
     const channelSecret = process.env.LINE_CHANNEL_SECRET;
