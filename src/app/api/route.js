8873a0358dfdcc3fb5fa7ae9098300857173041e
wavekasipat <wavekasipat@gmail.com>
2024-10-27 12:39:09 +0700

    hello api

---
+++ src/app/api/route.js
@@ -0,0 +1,11 @@
+export async function GET() {
+    console.log("GET /api Start");
+    return new Response("Hello, world!");
+}
+
+export async function POST(req) {
+    console.log("POST /api Start");
+    const body = await req.json();
+    console.log("POST /api body:", body);
+    return new Response("Hello, world!");
+}
